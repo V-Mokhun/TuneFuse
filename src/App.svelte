74f1e33689f804e9435f91287bed1f34eb7b5392
V-Mokhun <mohunmax1408@gmail.com>
2024-01-27 17:03:07 +0100

    Add button and card components from shadcn-svelte, add basic sign-in page and svelte-routing integration

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,41 @@
 <script lang="ts">
+  import { SignIn } from "@/pages";
+  import { buttonVariants } from "@/shared/ui";
+  import { Search } from "lucide-svelte";
+  import { Link, Route, Router } from "svelte-routing";
+
+  export let url = "";
 </script>
 
-<header></header>
-<main class="flex-1">
-  <h1 class="text-2xl text-red-400">Hello</h1>
-</main>
-<footer></footer>
+<Router {url}>
+  <header class="flex items-center justify-between h-20 px-4 md:px-6">
+    <Link class="flex items-center" to="/">
+      <span>TuneFuse</span>
+    </Link>
+    <div class="flex items-center justify-center w-full max-w-md mx-auto">
+      <form class="relative w-full">
+        <Search
+          size={24}
+          class="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400"
+        />
+        <input
+          type="search"
+          class="flex h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 pl-8 w-full"
+          placeholder="Search for songs, artists, albums..."
+        />
+      </form>
+    </div>
+    <div class="flex items-center gap-4">
+      <Link to="/sign-in" class={buttonVariants()}>Sign in</Link>
+    </div>
+  </header>
+  <main class="flex-1">
+    <Route path="/sign-in"><SignIn /></Route>
+    <Route path="/sign-up">Sign up</Route>
+    <Route path="/">Home</Route>
+  </main>
+  <footer></footer>
+</Router>
 
 <style>
 </style>
---
+++ src/pages/auth/sign-in/SignIn.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import {
+    Button,
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/shared/ui";
+  import { Link } from "svelte-routing";
+</script>
+
+<section>
+  <Card class="mx-auto max-w-sm">
+    <CardHeader>
+      <CardTitle class="text-xl">Sign In</CardTitle>
+      <CardDescription>Enter your email and password to sign in</CardDescription
+      >
+    </CardHeader>
+    <CardContent>
+      <form class="space-y-4">
+        <!-- <div class="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" placeholder="m@example.com" required type="email" />
+        </div>
+        <div class="space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" required type="password" />
+        </div> -->
+        <Button class="w-full" type="submit">Sign In</Button>
+        <Button class="w-full" variant="outline">Sign in with Google</Button>
+        <Button class="w-full" variant="outline">Sign in with GitHub</Button>
+        <Button class="w-full" variant="outline">Sign in with Spotify</Button>
+      </form>
+      <div class="mt-4 text-center text-sm">
+        Don't have an account?
+        <Link class="underline" to="/sign-up">Sign up</Link>
+      </div>
+    </CardContent>
+  </Card>
+</section>
+
+<style>
+</style>
